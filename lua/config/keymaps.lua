73cc28bdd745dea561a3e4f29936d5fd5af1feff
mohamed hosny <m.hosny@intrazero.net>
2025-08-30 03:42:40 +0300

    final;

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Remove LazyVim's default leader+e mapping
+
+vim.keymap.del("n", "<leader>e")
+-- vim.keymap.set("n", "<leader>e", "<cmd>call VSCodeNotify('revealInExplorer')<cr>", { desc = "Reveal in File Explorer" })
